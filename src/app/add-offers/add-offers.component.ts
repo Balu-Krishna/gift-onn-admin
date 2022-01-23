7155fcd277c38897860bfb9e0a20e8a6a4f340cd
Sudharshan Nallamothu <sudharshan.nallamotthu@oracle.com>
2022-01-23 22:29:02 +0530

    fix

--- src/app/add-offers/add-offers.component.ts
+++ src/app/add-offers/add-offers.component.ts
@@ -1,47 +1,30 @@
-import { Component, } from '@angular/core';
-import { OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-add-offers',
-  templateUrl: './add-offers.component.html',
-  styleUrls: ['./add-offers.component.scss']
+  selector: "app-add-offers",
+  templateUrl: "./add-offers.component.html",
+  styleUrls: ["./add-offers.component.scss"],
 })
 export class AddOffersComponent implements OnInit {
-
-
-  addOffersGroup: FormGroup
-
-
-
-
-
-
-
-
+  addOffersGroup: FormGroup;
 
   constructor(private fb: FormBuilder, private router: Router) {
     this.addOffersGroup = this.fb.group({
-
-      category: [""],
       offerName: [""],
-      description: [""],
-      date: [""],
-      storeId: [""],
       offerId: [""],
+      storeId: [""],
       offerCategoty: [""],
-      subCategory: [""],
-      SelectCountry: [""],
-      SelectLocation: [""],
-      Description: [""],
+      offerSubCategory: [""],
+      country: [""],
+      location: [""],
+      description: [""],
       startDate: [""],
-      EndDate: [""],
-    })
+      endDate: [""],
+    });
   }
 
-  ngOnInit(): void {
-  }
-  addOffersSubmit() {
-
-  }
+  ngOnInit(): void {}
+  addOffersSubmit() {}
 }
