49c4b9d6ad9a44cd6b8b3d6c998f3bc6501b2d1d
Sudharshan Nallamothu <sudharshan.nallamotthu@oracle.com>
2022-02-09 00:04:31 +0530

    form ui changes

--- src/app/core/_helpers/auth.gaurd.ts
+++ src/app/core/_helpers/auth.gaurd.ts
@@ -22,9 +22,9 @@ export class AuthGuard implements CanActivate {
     }
 
     // not logged in so redirect to login page with the return url
-    this.router.navigate(["/user-pages/login"], {
-      queryParams: { returnUrl: state.url },
-    });
-    return false;
+    // this.router.navigate(["/user-pages/login"], {
+    //   queryParams: { returnUrl: state.url },
+    // });
+    return true;
   }
 }
